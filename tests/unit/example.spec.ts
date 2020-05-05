310e0a682f96b2443510675d09dd56117569ebbd
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-05-05 05:01:02 -0700

    fix: use Actions import in example unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Actions from "@/components/home/Actions.vue";
 
-describe("HelloWorld.vue", () => {
+describe("Actions.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Actions, {
       propsData: { msg }
     });
     expect(wrapper.text()).to.include(msg);
